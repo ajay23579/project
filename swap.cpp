a247c34aa37ae7807fccfcba33ecdd7a28fa2732
ajay23579 <ajaysubedihtd7@gmail.com>
2025-05-19 07:34:05 +0545

    c++

---
+++ swap.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a =5;
+    int b =6;
+    int temp;
+    cout << "Before Swap"<<endl <<a <<endl <<b <<endl;
+    temp = a;
+    a = b;
+    b = temp;
+
+    cout << "After Swap"<<endl <<a <<endl <<b <<endl;
+    return 0;
+}
